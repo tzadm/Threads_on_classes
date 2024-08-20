6c391f6393f806097b47e9b2bb95b010ab3e6c7a
tzadm <94993624+tzadm@users.noreply.github.com>
2024-08-20 10:44:25 +0300

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -15,7 +15,12 @@ class Knight(Thread):
         while enemies != 0:
             enemies -= self.power
             days_of_battle += 1
+            if enemies <= 0:
+                enemies -= enemies
+                print(f'{self.name} сражается {days_of_battle} день(дня)..., осталось {enemies} воинов')
+                break
             print(f'{self.name} сражается {days_of_battle} день(дня)..., осталось {enemies} воинов')
+
             sleep(1)
         print(f'{self.name} одержал победу спустя {days_of_battle} дней(дня)!')
 
